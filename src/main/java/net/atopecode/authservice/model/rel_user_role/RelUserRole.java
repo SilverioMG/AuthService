9a5494bfe34b7f4e24a6f1d07f8ff9ef783925ba
Silverio Martinez Garcia <atopecode@gmail.com>
2021-05-17 18:41:38 +0200

    Added equals and hashcode method to entities

--- src/main/java/net/atopecode/authservice/model/rel_user_role/RelUserRole.java
+++ src/main/java/net/atopecode/authservice/model/rel_user_role/RelUserRole.java
@@ -69,6 +69,31 @@ public class RelUserRole {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RelUserRole other = (RelUserRole) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		String userString = (user != null) ? user.toString() : "null";
 		String roleString = (role != null) ? role.toString() : "null";
